a541be680c215e1d7e6f14d62431ac722a663aae
Sofia <sofia.salazar.m@utec.edu.pe>
2024-12-23 23:11:52 -0500

    feat: add routes.js and modify main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const routes = require("./routes");
 
-app.listen(port, () => {});
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
